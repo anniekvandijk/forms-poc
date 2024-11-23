60c70c3ccbb49143b70b602cb62e0b77f136b641
Anniek van Dijk <anniek@animundo.nl>
2024-11-23 16:01:10 +0100

    add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MainformComponent } from "./forms/mainform/mainform.component";
+import { MainformComponent } from './forms/mainform/mainform.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, MainformComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [MainformComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'forms-poc';
